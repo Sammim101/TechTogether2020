34dcc7bbdeb1e3b1ff30d2ce36bb5ad0c93efbc7
Angela Li <angelali@Angelas-MacBook-Air.local>
2020-11-07 17:29:16 -0500

    changed logic

--- logic.py
+++ logic.py
@@ -4,6 +4,14 @@ import data_credit
 import data_invest
 import data_loans
 import random
+import copy
+
+# data backup due to deletiion during the getQuestionInf()
+data_budget_copy = copy.deepcopy(data_budget.data)
+data_bill_copy = copy.deepcopy(data_bill.data)
+data_loan_copy = copy.deepcopy(data_loan.data)
+data_credit_copy = copy.deepcopy(data_credit.data)
+data_invest_copy = copy.deepcopy(data_invest.data)
 
 def getQuestionInfo(category):
     data = data_budget.data
@@ -27,14 +35,33 @@ def getQuestionInfo(category):
 
     return random_question
 
-def checkAnswer(quiz, answer):
-    #todo: check if the answer is correct for the quiz and record the proceeding
-    print("todo")
+def checkAnswer(category, question, answer):
+    accuracy = false
+
+
+
+
+
     return
 
 def getScore():
     #todo: get the score 
     print("todo")
     return
+
+def determineData(category):
+        data = data_budget.data
+    if category == "budget":
+        data = data_budget.data
+    elif category == "bill":
+        data = data_bill.data
+    elif category == "loan": 
+        data = data_loan.data
+    elif category == "credit":
+        data = data_credit.data
+    elif category == "invest":
+        data = data_invest.data
+
+
 
 
